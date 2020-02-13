0c46051335365c76840aecb93b45a3035de15d7d
JonathanCauchi <33484962+JonathanCauchi@users.noreply.github.com>
2020-02-13 16:52:52 +0100

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -12,33 +12,28 @@ int i,j,temp;
 for(i=0;i<a_size;i++)
 {
 
-for(j=i+1;j<a_size;j++)
-{
+  for(j=i+1;j<a_size;j++)
+  {
 
-if(a[i]>a[j])
-{
-temp = a[i];
-a[i] = a[j];
-a[j] = temp;
-}
-else
-{
-continue;
-}
+    if(a[i]>a[j])
+    {
+      temp = a[i];
+      a[i] = a[j];
+      a[j] = temp;
+    }
+    else
+    {
+      continue;
+    }
 
 //loop--;
-
-
-
-
-
 }
 
 }
 
 for(int c=0; c<a_size; c++)
 {
-printf("%d",a[c]);
+  printf("%d",a[c]);
 }
 
 }
