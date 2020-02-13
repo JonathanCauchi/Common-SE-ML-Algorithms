a8da35236bce5a448b93a8f9ec2b90461a936517
JonathanCauchi <33484962+JonathanCauchi@users.noreply.github.com>
2020-02-13 15:09:19 +0100

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+
+int a[]= {3,2,6,5,7,1};
+int a_size = sizeof(a)/sizeof(int);
+int i,j,temp;
+
+//int loop = a_size;
+
+for(i=0;i<a_size;i++)
+{
+
+for(j=i+1;j<a_size;j++)
+{
+
+if(a[i]>a[j])
+{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+}
+else
+{
+continue;
+}
+
+//loop--;
+
+
+
+
+
+}
+
+}
+
+for(int c=0; c<a_size; c++)
+{
+printf("%d",a[c]);
+}
+
+}
