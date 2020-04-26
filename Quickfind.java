f00cfe15c7e0980dd8050ef536949d97665efe60
JonathanCauchi <33484962+JonathanCauchi@users.noreply.github.com>
2020-04-26 16:48:01 +0000

    Add files via upload

---
+++ Quickfind.java
@@ -0,0 +1,32 @@
+
+public class Algo1
+{  
+    private int[] id;
+    
+    public Algo1(int n)
+    {  
+     id = new int[n];
+     for(int i = 0; i<n; i++)
+        id[i] = i;
+    }  
+    
+    public void union(int p, int q)
+    {
+        int pid = id[p];
+        int qid = id[q];
+        for(int i =0; i < id.length; i++)
+            {
+                if(id[i] == pid) 
+                {
+                    id[i] = qid;
+                }
+            }
+                
+            
+    }
+    
+    public boolean connected(int p, int q)
+    {
+        return id[p] == id[q];
+    }
+}  
