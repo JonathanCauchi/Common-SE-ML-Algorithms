e4d50d416d9ff41c1ff87bd38f41c3365ce25053
JonathanCauchi <33484962+JonathanCauchi@users.noreply.github.com>
2020-05-23 15:37:23 +0200

    Add files via upload

---
+++ Fun basic exercises/starsequencereverse.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+int num;
+printf("Enter number of layers: \n");
+scanf("%d",&num);
+for(int i = 1; i <= num; i++)
+{
+	for(int j = num; j>=i ; j--)
+	{
+	printf(" ");
+	}
+	for(int k=1; k<=i; k++)
+	{
+	printf("*");
+	}	
+	printf("\n");
+}
+return 0;
+}
