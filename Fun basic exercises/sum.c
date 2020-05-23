27b70794f97a69dfb6def981fe7bccbc99ccbaff
JonathanCauchi <33484962+JonathanCauchi@users.noreply.github.com>
2020-05-23 13:51:23 +0200

    Add files via upload

---
+++ Fun basic exercises/sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+long int num1,num2;
+printf("Enter number 1:\n ");
+scanf("%ld",&num1);
+printf("Enter number 2:\n ");
+scanf("%ld",&num2);
+long int result = num1+num2;
+printf("Sum is equal to: %ld ", result);
+return 0;
+}
+
+
