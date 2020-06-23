37e51f54ec6070f647d5211fb1824c21b93e7323
JonathanCauchi <33484962+JonathanCauchi@users.noreply.github.com>
2020-06-23 17:11:59 +0200

    Add files via upload

---
+++ Python DataStruc/Euler's Problems/largestprimefactor.py
@@ -0,0 +1,21 @@
+
+def isprime(num):
+	for i in range(2, int(num**0.5)+1):
+		if(num % i == 0):
+			return False
+	return True
+
+def main():
+	num = 600851475143
+	list_prime = []
+	for i in range(2,num+1):
+		if(isprime(i)==True):
+			if(num % i == 0):
+				list_prime.append(i)
+			else:
+				continue
+
+	print(max(list_prime))
+		
+	
+main()
