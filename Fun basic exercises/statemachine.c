1c1075b15b4d01d6f1aa9fdd87a5d56ab6f2a134
JonathanCauchi <33484962+JonathanCauchi@users.noreply.github.com>
2020-06-27 19:23:57 +0200

    Add files via upload

---
+++ Fun basic exercises/statemachine.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+enum state
+{
+idle = 0,
+waiting = 1,
+being_served = 2,
+exiting = 3
+};
+
+int main()
+{
+int state = idle;
+int flag = 0;
+while(flag == 0)
+{
+int option;
+printf("What is your current state?\n");
+printf("1-Waiting, 2-Being served, 3-Exiting\n");
+scanf("%d",&option);
+if(option == 1)
+{
+	state = waiting;
+	printf("State is waiting\n");
+	printf("%d\n",option);
+}
+if(option == 2)
+{
+	state = being_served;
+	printf("State is being served\n");
+	printf("%d\n",state);
+}	
+if(option == 3)
+{
+	state = exiting;
+        printf("Exiting\n");
+        printf("%d\n",state);
+        exit(0);
+}
+if(option < 1 && option > 3)
+{
+	printf("Invalid input\n");
+}
+
+}
+
+
+
+
+return 0;
+}
+
